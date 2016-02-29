ff63f10bab9da21d94982a4e42fcac3bbcf2df62
Surya Teja <suryasteja59@gmail.com>
2016-02-29 20:33:17 +0530

    added splash screen

---
+++ app/src/main/java/com/votzapp/app/Splash.java
@@ -0,0 +1,29 @@
+package com.votzapp.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handel = new Handler();
+        handel.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent loadSplash = new Intent(Splash.this, MainActivity.class);
+
+                startActivity(loadSplash);
+
+                finish();
+            }
+        }, 5000);
+    }
+}
